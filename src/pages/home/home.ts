7a258dfd6edfb5bf74caecf39b585a476be11de3
Fagner Souza <fagner.roberto89@gmail.com>
2021-10-10 12:48:03 -0300

    Salvando os dados do usuario logado no localStorage

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -33,9 +33,9 @@ export class HomePage {
   login(){
     this.auth.authenticate(this.creds)
       .subscribe(response => {
-        console.log(response.headers.get('Authorization'));
+        this.auth.successfulLogin(response.headers.get('Authorization'));
         this.navCtrl.setRoot('CategoriasPage');
       },
-      error => {});    
+      error => {});
   }
 }
