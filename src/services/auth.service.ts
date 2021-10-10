9608244dc1b8186ec78ba6ed087971a413f96864
Fagner Souza <fagner.roberto89@gmail.com>
2021-10-10 13:44:44 -0300

    Extraindo o email do token e armazenando-o em localStorage-corrigindo erro

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,38 +1,39 @@
-import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { API_CONFIG } from "../config/api.config";
 import { CredenciaisDTO } from "../models/credenciais.dto";
+import { HttpClient } from "@angular/common/http";
+import { API_CONFIG } from "../config/api.config";
 import { LocalUser } from "../models/local_user";
 import { StorageService } from "./storage.service";
-import { JwtHelperService } from "@auth0/angular-jwt";
+import { JwtHelper } from 'angular2-jwt';
 
 @Injectable()
 export class AuthService {
- 
-    jwtHelperService: JwtHelperService = new JwtHelperService();
- 
-    constructor(public http: HttpClient, public storage: StorageService){}
- 
-    authenticate(creds: CredenciaisDTO) {
+
+    jwtHelper: JwtHelper = new JwtHelper();
+
+    constructor(public http: HttpClient, public storage: StorageService) {
+    }
+
+    authenticate(creds : CredenciaisDTO) {
         return this.http.post(
-            `${API_CONFIG.baseUrl}/login`,
+            `${API_CONFIG.baseUrl}/login`, 
             creds,
             {
                 observe: 'response',
                 responseType: 'text'
             });
     }
- 
-    successfulLogin(authorizationValue : string){
+
+    successfulLogin(authorizationValue : string) {
         let tok = authorizationValue.substring(7);
         let user : LocalUser = {
             token: tok,
-            email: this.jwtHelperService.decodeToken(tok).sub
+            email: this.jwtHelper.decodeToken(tok).sub
         };
         this.storage.setLocalUser(user);
     }
- 
-    logout(){
+
+    logout() {
         this.storage.setLocalUser(null);
     }
 }
