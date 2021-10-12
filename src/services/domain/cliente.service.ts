dde4948b0d699cef04bdbe8b00da87001bb31cc7
Fagner Souza <fagner.roberto89@gmail.com>
2021-10-12 17:40:11 -0300

    Tratamento padrao para outros erros com Alert

--- src/services/domain/cliente.service.ts
+++ src/services/domain/cliente.service.ts
@@ -1,18 +1,17 @@
-import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { Observable } from "rxjs";
-import { API_CONFIG } from "../../config/api.config";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs/Rx";
 import { ClienteDTO } from "../../models/cliente.dto";
+import { API_CONFIG } from "../../config/api.config";
 import { StorageService } from "../storage.service";
 
 @Injectable()
 export class ClienteService {
 
-    constructor(public http: HttpClient, public storage: StorageService) {        
+    constructor(public http: HttpClient, public storage: StorageService) {
     }
 
     findByEmail(email: string) : Observable<ClienteDTO> {
-
         return this.http.get<ClienteDTO>(`${API_CONFIG.baseUrl}/clientes/email?value=${email}`);
     }
 
